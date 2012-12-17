aa16447ce61fa84af4535497b586fca0ce820b2d
Justin Ouellette <justin.ouellette@gmail.com>
2012-12-17 14:25:14 -0500

    pragma

--- JXHTTP.h
+++ JXHTTP.h
@@ -20,6 +20,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#pragma HC SVNT DRACONES
+
 // Core
 #import "JXOperation.h"
 #import "JXURLConnectionOperation.h"
