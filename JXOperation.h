435abbd32437e5d8c9c0b07220b54cf3daca2cb8
Justin Ouellette <justin.ouellette@gmail.com>
2013-03-08 16:47:11 -0500

    jxoperation comment updates

--- JXOperation.h
+++ JXOperation.h
@@ -1,25 +1,26 @@
 /**
  `JXOperation` is an abstract `NSOperation` subclass that implements all the
  methods necessary for what Apple calls "concurrent" operations (see the sections
- titled "Subclassing Notes" and "Multicore Considerations" in the `NSOperation`
+ titled _Subclassing Notes_ and _Multicore Considerations_ in the `NSOperation`
  class reference, this subclass does all of it for you.)
  
  The main advantage of concurrent operations is that they allow for the use of
  asynchronous APIs. Normally, when the `main` method of an `NSOperation` exits,
- the operation flags itself as finished and deallocation is imminent. In a
+ the operation marks itself as finished and deallocation is imminent. In a
  concurrent operation, nothing happens when `main` exits and it's up to you to
- manually report that the operation is finished via KVO. In the meantime you can
- wait for delegate callbacks or do anything else that requires waiting for the
- runloop to turn over.
+ manually report that the operation has finished (via KVO). In the meantime you
+ can wait for delegate callbacks or any other asynchronous event.
  
  Unfortunately, `NSOperation` has some quirks and ensuring thread safety as
  the operation changes state can be tricky. `JXOperation` makes it easy by
- providing a simple `finish` method that can be called at any time from any
+ providing a simple <finish> method that can be called at any time from any
  thread without worrying about the operation's current state.
  
  Heavily inspired by Dave Dribin, and building on his work detailed here:
- 
+
  <http://dribin.org/dave/blog/archives/2009/05/05/concurrent_operations>
+
+ <http://dribin.org/dave/blog/archives/2009/09/13/snowy_concurrent_operations>
  */
 
 @interface JXOperation : NSOperation
@@ -27,7 +28,7 @@
 /// @name Operation State
 
 /**
- `YES` while the operation is executing and `NO` once it has finished.
+ `YES` while the operation is executing, otherwise `NO`.
  
  Safe to access from any thread at any time.
  */
@@ -43,9 +44,11 @@
 /**
  Upon being set to `YES`, retrieves a `UIBackgroundTaskIdentifier` to cause the
  operation to continue running when the application enters the background.
- Changing this property to `YES` after the operation starts has no effect.
  
  Safe to access from any thread at any time.
+ 
+ @warning Changing this property to `YES` after the operation has started will
+ have no effect. Changing it to `NO` will discontinue background execution.
  */
 @property (assign) BOOL continuesInAppBackground;
 
@@ -58,7 +61,7 @@
  */
 + (instancetype)operation;
 
-/// @name Starting and Finishing
+/// @name Starting & Finishing
 
 /**
  Starts the operation and blocks the calling thread until it has finished.
@@ -67,7 +70,7 @@
 
 /**
  Subclasses should override this method (and call super) rather than overriding
- `finish`. In addition, this method should never be called manually. It is
+ <finish>. In addition, this method should never be called manually. It is
  guaranteed to be called exactly one time from one thread just before the operation
  is marked as finished (and thus potentially deallocated). If your operation
  has any cleanup to do before it disappears forever, do it here.
@@ -75,14 +78,17 @@
 - (void)willFinish;
 
 /**
- Ends the operation. Subclasses must eventually call this method to cause the
- operation to finish (typically at the end of their main method). To ensure thread
- saftey, do not override this method (use `willFinish` instead, see above). Once
- the operation has finished to do not attempt to access any of its properties as it
- may be released by a queue or other retaining object from a different thread.
+ Ends the operation and marks it for removal from its queue (if applicable).
+ Subclasses must eventually call this method to cause the operation to finish,
+ typically after the work performed in `main` is complete.
  
  This method is safe to call multiple times from any thread at any time, including
  before the operation has started.
+ 
+ @warning To ensure thread saftey, do not override this method (use <willFinish>
+ instead). Once the operation has finished to do not attempt to access any of its
+ properties as it may be released by a queue or other retaining object from a
+ different thread.
  */
 - (void)finish;
 
