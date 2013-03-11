86be883b4d492af3bdb1a4253c0a07623eb6924b
Justin Ouellette <justin.ouellette@gmail.com>
2013-03-11 16:29:19 -0400

    example app

---
+++ example/JXExample/JXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JXAppDelegate.h
+//  JXExample
+//
+//  Created by Justin Ouellette on 3/11/13.
+//  Copyright (c) 2013 JXHTTP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ example/JXExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JXExample
+//
+//  Created by Justin Ouellette on 3/11/13.
+//  Copyright (c) 2013 JXHTTP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JXAppDelegate class]));
+    }
+}
